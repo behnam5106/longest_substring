e0fefbfbb89b299e83c0713f8e135bbd6c8ade87
behnam5106 <47812522+behnam5106@users.noreply.github.com>
2024-03-21 08:37:25 +0100

    final test

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -1,26 +1,27 @@
 class Solution:
     def lengthOfLongestSubstring(self, s:str) -> int:
-        s_unicMember=""
         s_sub=""
         for k in range(0,len(s)):
-            n=len(s)-k
-          
+            #n=len(s)-k 
+            print("len(s):",len(s))   
+            print("k:",k)     
             for j in range(0,k+1):
-                s_sub=s[k:n]
-                print(s_sub)
-                for i in range(0,len(s_sub)):
-                    if s_unicMember.find(s_sub[i])==-1:
-                        s_unicMember=s_unicMember+s_sub[i]
-                    if len(s_unicMember)==len(s_sub):
-                        return len(s_unicMember)
-
-
-
-s="abcdabbbbbn"
+                s_sub=s[j:len(s)-k+j]
+                print("j:",j)
+                print("s_sub:",s_sub, "len(s_sub):",len(s_sub))
+                x=0
+                for i in s_sub:
+                    print("i:",i)
+                    if s_sub.count(i)==1:
+                        x=x+1
+                        print("x:",x)
+                if x==len(s_sub):
+                    print("OK")
+                    return x
+ 
+s="1233456sg//s;js;ojssoustu957ll4-9;gmgslkg1ss5f"
 
 solution=Solution()
-
 print(solution.lengthOfLongestSubstring(s))
-
-
+     
 
